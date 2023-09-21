b4ccdcfe0b92745cd39f56e16125e94ec7d3e7cd
fahmida43 <99779584+fahmida43@users.noreply.github.com>
2023-09-21 20:33:09 +0600

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+$( function() {
+    $('#banner').slick({
+        arrows: false,
+        dots: true,
+        dotsClass: "container bannerDots",
+        autoplay:true,
+        autoplaySpeed:2500,
+
+      });
+
+})
